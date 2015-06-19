82f9b91a1811361284f830604c00059670293fb5
izrik <izrik@izrik.com>
2015-06-19 13:27:04 -0500

    Initial project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SemVerAdvisor")]
+[assembly: AssemblyDescription("Compare two versions of an assembly and suggest how to bump version numbers")]
+[assembly: AssemblyCopyright("izrik")]
+
+[assembly: AssemblyVersion("0.1")]
+
